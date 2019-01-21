7ccf1647d7cf9e21a1509f7c320dd2beb44611e8
igradeca <gradecak.ivan@gmail.com>
2019-01-21 21:05:35 +0100

    Added inventory opening and closing.

---
+++ Inventory system/Assets/Scripts/InventoryScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryScript : MonoBehaviour {
+
+    public GameObject inventoryPanel;
+    public GameObject inventoryButton;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+
+    public void ShowPanel() {
+
+        if (inventoryPanel.activeSelf == true) {
+            inventoryPanel.SetActive(false);
+            inventoryButton.SetActive(true);
+            
+        } else {
+            inventoryPanel.SetActive(true);
+            inventoryButton.SetActive(false);
+        }
+    }
+
+}
---
+++ Inventory system/Assets/Scripts/UIScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour {
+
+    public GameObject panelAttributes;
+    public GameObject panelEquipment;
+    public GameObject panelInventory;
+
+    public Button buttonAttributes;
+    public Button buttonEquipment;
+    public Button buttonInventory;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
